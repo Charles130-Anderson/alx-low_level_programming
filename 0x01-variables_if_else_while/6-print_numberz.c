ae15ebc517d4e2cb6c439a0a0e9b5e7fd62a932a
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-08-11 10:33:00 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers from 0 to 9.
+ * main - Prints digits from 0 through 9.
  *
- * Return: Always 0 (Success)
+ * Return: Always return 0 when successful
  */
 int main(void)
 {
