8ab7f0056da81af647280cafb9fa3d262b3bf65f
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-08-11 09:20:48 -0700

    corrections

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,26 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Classifies the number 7.
+ * main - Determines if a number is positive, negative or zero.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int myNumber = 7; /* Set the number to 7 */
+	int n;
 
-    if (myNumber > 0)
-    {
-        printf("%d is positive\n", myNumber);
-    }
-    else if (myNumber == 0)
-    {
-        printf("%d is zero\n", myNumber);
-    }
-    else
-    {
-        printf("%d is negative\n", myNumber);
-    }
-
-    return 0;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
 }
