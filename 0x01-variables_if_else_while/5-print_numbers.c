ef6b3b6e8ae474d960e46eb8741d2fbd7df56b99
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-08-11 10:13:44 -0700

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers from 0 to 9.
+ * main - Prints all numbers from 0 to 9.
  *
- * Return: Always 0 (Success)
+ * Return: (0) Successful
  */
 int main(void)
 {
