89a658eb75e9a416d2bc67c4d9748d0ba92c2c92
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-08-30 07:32:21 -0700

    task 2 correction

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,10 +9,12 @@
  */
 #include <stdio.h>
 
-int _strlen_recursion(char *s) {
-if (*s == '\0') {
-return 0;
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return (0);
 }
 
-return 1 + _strlen_recursion(s + 1);
+return (1 + _strlen_recursion(s + 1));
 }
