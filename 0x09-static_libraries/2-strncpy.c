52a76afddaf6dbcab79c65a218c7b798026cf583
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-09-01 09:11:17 -0700

    corrections

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings up to n characters.
+ * @dest: The destination string.
+ * @src: The source string.
+ * @n: The maximum number of characters to concatenate.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+
+return (dest);
+}
