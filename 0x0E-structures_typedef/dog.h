a71bbe4dea7d727ec42a1cf47201f0b69123b029
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-09-11 09:58:13 -0700

    task 0 ,defining a new type 'struct dog'

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define Dog_H
+ /** struct dog - the dog's basic info
+ * @name:The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ * Description: Furst struct
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
