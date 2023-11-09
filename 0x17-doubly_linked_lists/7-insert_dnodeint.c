02938e9431171a1bbcb32f7969907a102188a906
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-11-09 04:27:23 -0800

    This function works by using a while loop to iterate over the list until it reaches the desired index, creating a new node and adjusting the next and prev pointers to insert it at the correct position. The function returns a pointer to the new node

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position in a dlistint_t list
+ * @h: double pointer to the head of the list
+ * @idx: index of the list where the new node should be added
+ * @n: integer to be stored in the new node
+ *
+ * Return: the address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node, *current;
+	size_t i = 0;
+
+	if (idx == 0)
+	{
+		add_dnodeint(h, n);
+		return (*h);
+	}
+
+	current = *h;
+	while (current != NULL && i < idx - 1)
+	{
+		i++;
+		current = current->next;
+	}
+
+	if (i == idx - 1)
+	{
+		new_node = malloc(sizeof(dlistint_t));
+		if (new_node == NULL)
+			return (NULL);
+
+		new_node->n = n;
+		new_node->prev = current;
+		new_node->next = current->next;
+		if (current->next != NULL)
+			current->next->prev = new_node;
+		current->next = new_node;
+
+		return (new_node);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
