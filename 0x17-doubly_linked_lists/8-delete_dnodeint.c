a6f67c9071b502aef0be8978bef8f1ffe6c46749
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-11-09 06:13:02 -0800

    This function works by using a while loop to iterate over the list until it reaches the desired index, and then removing the node at that position. The function returns 1 to indicate success, or -1 to indicate failure

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index - deletes the node at a given index of a dlistint_t list
+ * @head: double pointer to the head of the list
+ * @index: index of the node that should be deleted
+ *
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current, *temp;
+	size_t i = 0;
+
+	current = *head;
+	while (current != NULL && i < index)
+	{
+		i++;
+		current = current->next;
+	}
+
+	if (i == index)
+	{
+		temp = current->next;
+		if (current->prev != NULL)
+			current->prev->next = temp;
+		if (temp != NULL)
+			temp->prev = current->prev;
+		if (current == *head)
+			*head = temp;
+		free(current);
+		return (1);
+	}
+	else
+	{
+		return (-1);
+	}
+}
