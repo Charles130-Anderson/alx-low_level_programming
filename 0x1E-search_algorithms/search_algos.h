29d2cdd2ed0a6ee848cf907f695b141bfafe69d5
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-04-14 08:43:18 -0700

    search_algorithms

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -22,6 +22,22 @@ typedef struct skiplist_s
     struct skiplist_s *express;
 } skiplist_t;
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
 /* Function prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
@@ -34,4 +50,5 @@ void print_array(int *array, size_t left, size_t right);
 int advanced_binary_recursive(int *array, size_t left, size_t right, int value);
 int advanced_binary(int *array, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 #endif /* SEARCH_ALGOS_H */
